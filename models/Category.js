89f0aeee4face29962ac01dc4fee69f2b23282aa
russelldnn <russelldnn@gmail.com>
2022-07-25 07:33:21 -0500

    category model defined

--- models/Category.js
+++ models/Category.js
@@ -6,8 +6,24 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement:true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    
+
+
   },
+
+
+
   {
     sequelize,
     timestamps: false,
